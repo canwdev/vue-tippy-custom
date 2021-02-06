7a44beb8405a938b7faf1f7f61c517bced38de8f
kenny <c2097@live.com>
2021-02-06 11:29:25 +0800

    tippy

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 Vue.config.productionTip = false
+import "tippy.js/themes/light-border.css";
+
+import VueTippy, {TippyComponent} from 'vue-tippy'
+Vue.use(VueTippy)
+Vue.component('tippy', TippyComponent)
 
 new Vue({
   router,
